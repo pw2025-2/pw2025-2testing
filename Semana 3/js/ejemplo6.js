3c0fcfa8b623f5764317d52f7ae53718fe762f1e
cabrerajumi <cabrerajumi@ouotloo.com>
2025-09-01 20:54:51 -0500

    Clase 01-09

---
+++ Semana 3/js/ejemplo6.js
@@ -0,0 +1,8 @@
+
+
+var rpta = suma(2,8);
+document.writeln("<p> La suma es :" + rpta + "</p>");
+
+function suma(a,b){
+    return a+b;
+}
---
+++ Semana 3/js/ejercicio.js
@@ -0,0 +1,16 @@
+var opcion = prompt("Ingresa un valor del 1 al 5");
+
+var letra1 = "<p>elefante se balanceaba sobre la tela de una araña</p>"
+var letra2 = "<p>Como veia que resistia fue a llamar a otro elefantes mas..</p>"
+
+switch (opcion){
+    case "1":
+        document.writeln("<strong>Un</strong>"+letra1);
+        break;
+    case "2":
+        for(let i = 1; i ==opcion; i++)
+        document.writeln(letra1) 
+         
+        break;
+     
+  }
